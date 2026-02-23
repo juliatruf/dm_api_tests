2fb02a623022a8f8492211742422e297217ad1bc
Julia Trufanova <julia.rvrn@gmail.com>
2026-02-23 01:01:05 +0100

    добавлены новые негативные тесты

--- tests/functional/delete_v1_account_login/test_delete_v1_account_login.py
+++ tests/functional/delete_v1_account_login/test_delete_v1_account_login.py
@@ -1,7 +1,10 @@
+from checkers.http_checkers import check_status_code_http
+
+
 def test_delete_v1_account_login(auth_account_helper):
     """Проверка инвалидирования токена текущей сессии после logout"""
     # Выход из аккаунта
     auth_account_helper.logout_current_session()
     # Попытка получить данные пользователя с инвалидированным токеном
-    response = auth_account_helper.get_current_user_info(validate_response=False)
-    assert response.status_code == 401, "Токен текущей сессии не инвалидирован после logout"
+    with check_status_code_http(401, "User must be authenticated"):
+        auth_account_helper.get_current_user_info()
